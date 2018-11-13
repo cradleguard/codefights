241d47fdf7453c58278c8283b6c65b6bdb21107f
Alexey Grigoryev <grigoryev@sovacapital.com>
2018-11-13 17:37:09 +0300

    ReverseParantheses complete

--- ReverseParentheses.go
+++ ReverseParentheses.go
@@ -1,24 +1,18 @@
 package main
 
 func reverseParentheses(s string) string {
-	ret := make([]rune, 0)
 	var openBracketsIndex stack
 	for i := 0; i < len(s); i++ {
 		v := rune(s[i])
 		if v == '(' {
 			openBracketsIndex.push(i)
 		} else if v == ')' {
-			startPosition := openBracketsIndex.pop()
-			for k:= i - 1; k > startPosition; k-- {
-				ret = append(ret, rune(s[k]))
-			}
+			startPosition := openBracketsIndex.pop()			
+			s =  s[:startPosition] + reverse(s[startPosition + 1 : i]) + s[i + 1 : ]
 			i -= 2
-			s =  s[:startPosition] + s[startPosition + 1 :i] + s[i + 1 : ]
-		} else if len(openBracketsIndex) == 0 {
-			ret = append(ret,v)
-		}
+		} 
 	}
-	return string(ret)
+	return s
 }
 
 type stack []int
@@ -31,4 +25,12 @@ func (s *stack) pop() int {
 	ret := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
 	return ret
+}
+
+func reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
